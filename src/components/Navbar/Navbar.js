c31999d46d35cbfb8703d79524f2e5bc29200089
nguyenvy123 <nguyenvyqn21@gmail.com>
2020-08-18 11:21:50 +0700

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Navbar.scss";
+import logo from "../../logo.png";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <img src={logo} alt ="city tour logo"/>
+      <ul className="nav-links">
+        <li>
+          <a href="/" className="nav-link">
+           Home
+          </a>
+        </li>
+        <li>
+          <a href="/" className="nav-link">
+           About
+          </a>
+        </li>
+        <li>
+          <a href="/" className="nav-link">
+           Tours
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
